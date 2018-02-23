0d456632dff070ecce7139e490b096eaf10be5f5
Zoelie DeCola <zdecola@cristoreyny.org>
2018-02-23 13:55:48 +0000

    final

--- js/script.js
+++ js/script.js
@@ -5,17 +5,21 @@
 // CREATE THE FUNCTIONS BELOW
 
 // Document Ready Function. All of your jQuery should go in here. 
-$( document ).ready(function() {
-  
-$("#submit").click(function() {
-var userSays = $("#input").val();
-$("#message").text(userSays + "ay"); 
-});
+$(document).ready(function() {
 
-var input = $("#input").val();
-        var wordArray = input.split(" ");
-        for(i=O; i < wordArray.length; i++)
-		var word = wordArray[i];
+    $("#submit").click(function() {
+        var userSays = $("#input").val();
+        $("#message").text(userSays + "ay");
+        
+         var input = $("#input").val();
+    var wordArray = input.split(" ");
+    alert(wordArray.length)
+    for (var i = O; i < wordArray.length; i++){
+        var word = wordArray[i];
+    alert(word);}
+    });
+
+   
 
 });
 
@@ -25,7 +29,5 @@ var input = $("#input").val();
 
 
 // Create the sentenceToPigLatin function that takes a sentence as a parameter
-	//Loops through all the words in the sentence and transforms each word
-	//It should return a transfromed sentance
-
-
+//Loops through all the words in the sentence and transforms each word
+//It should return a transfromed sentance
